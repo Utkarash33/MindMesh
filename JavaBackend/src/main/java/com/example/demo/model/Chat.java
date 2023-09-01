5a6c225ad76b62437d857a6465e3f04ff7888c6f
sudharshan2151 <123924081+sudharshan2151@users.noreply.github.com>
2023-09-01 11:55:22 +0530

    basics

---
+++ JavaBackend/src/main/java/com/example/demo/model/Chat.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import java.util.List;
+
+public class Chat {
+	List<Prompt> chats;
+
+	public Chat() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Prompt> getChats() {
+		return chats;
+	}
+
+	public void setChats(List<Prompt> chats) {
+		this.chats = chats;
+	}
+	
+	
+
+	
+	
+}
